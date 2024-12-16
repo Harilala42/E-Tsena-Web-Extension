41453bb5c43d31a4995597dad87df0b89df2cb98
Niriantsoa Harilala Rasamimanana <nrasamim@c1r10s10.42antananarivo.mg>
2024-12-16 16:17:55 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# E-Tsena Web Entension
+
+Web Entension permettant aux utilisateurs à Madagascar 🇲🇬 :
+- Commander 🛒 directement sur Aliexpress
+- Payer 💳 en Ariary en Mobile Money (avec Mvola)
+- Suivre 🖲️ l'état en temps réel de la commande
---
+++ src/router.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+import Shopping from './components/shopping_card.vue';
+import Tracking from './components/order_tracking.vue';
+import Settings from './components/profil.vue';
+
+const routes = [
+    { path: '/shopping_card', name: 'Shopping', component: Shopping },
+    { path: '/order_tracking', name: 'Tracking', component: Tracking },
+    { path: '/profil', name: 'Settings', component: Settings }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
+
+export default router;
