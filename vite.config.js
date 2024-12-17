04cabadcb3444d92b58a5844bba07c16a204fc72
Niriantsoa Harilala Rasamimanana <nrasamim@c1r13s1.42antananarivo.mg>
2024-12-17 13:33:54 +0300

    on Chrome Web Store

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+"use strict";
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/manifest.json', dest: '' },
+        { src: 'public/icons/*', dest: 'icons' },
+        { src: 'public/fonts/*', dest: 'fonts' }
+      ]
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background.js')
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === 'background'
+            ? 'background.js'
+            : 'src/[name].js';
+        }
+      }
+    },
+    outDir: 'dist',
+    assetsDir: 'src'
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  }
+});
